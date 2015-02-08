6b3d07a388af7bbbd796a4ff5546c43992b827d1
wolpi <wolpi.dev@web.de>
2015-02-08 18:05:29 +0100

    basic impl of ssh server, not tested, just compilable

--- primitiveFTPd/src/org/primftpd/filesystem/AndroidFileSystemView.java
+++ primitiveFTPd/src/org/primftpd/filesystem/AndroidFileSystemView.java
@@ -2,45 +2,40 @@ package org.primftpd.filesystem;
 
 import java.io.File;
 
-import org.apache.ftpserver.ftplet.FileSystemView;
 import org.apache.ftpserver.ftplet.FtpException;
-import org.apache.ftpserver.ftplet.FtpFile;
-import org.apache.ftpserver.ftplet.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AndroidFileSystemView implements FileSystemView {
+import android.os.Environment;
 
-	private static final Logger logger = LoggerFactory.getLogger(AndroidFileSystemView.class);
+public abstract class AndroidFileSystemView<T extends AndroidFile<X>, X> {
 
-	private final User user;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	private AndroidFtpFile workingDir;
+	private T workingDir;
 
-	public AndroidFileSystemView(User user) {
-		this.user = user;
-		workingDir = createHomeDirObj(user);
+	public AndroidFileSystemView() {
+		workingDir = createHomeDirObj();
 	}
 
-	private AndroidFtpFile createHomeDirObj(User user) {
-		return new AndroidFtpFile(new File(user.getHomeDirectory()), user);
+	protected abstract T createFile(File file);
+
+	private T createHomeDirObj() {
+		File androidHomeDir = Environment.getExternalStorageDirectory();
+		return createFile(androidHomeDir);
 	}
 
-	@Override
-	public FtpFile getHomeDirectory() throws FtpException {
+	public T getHomeDirectory() {
 		logger.debug("getHomeDirectory()");
-		return createHomeDirObj(user);
+		return createHomeDirObj();
 	}
 
-	@Override
-	public FtpFile getWorkingDirectory() throws FtpException {
+	public T getWorkingDirectory() {
 		logger.debug("getWorkingDirectory()");
-
 		return workingDir;
 	}
 
-	@Override
-	public boolean changeWorkingDirectory(String dir) throws FtpException {
+	public boolean changeWorkingDirectory(String dir) {
 		logger.debug("changeWorkingDirectory({})", dir);
 
 		File dirObj = new File(dir);
@@ -54,19 +49,18 @@ public class AndroidFileSystemView implements FileSystemView {
 			path = workingDir.getAbsolutePath() + File.separator + dir;
 		}
 
-		workingDir = new AndroidFtpFile(new File(path), user);
+		workingDir = createFile(new File(path));
 
 		return true;
 	}
 
-	@Override
-	public FtpFile getFile(String file) throws FtpException {
+	public T getFile(String file) {
 		logger.debug("getFile({})", file);
 
 		File fileObj = new File(file);
 		if (fileObj.isAbsolute()) {
 			logger.debug("getFile(), returning abs: '{}'", file);
-			return new AndroidFtpFile(fileObj, user);
+			return createFile(fileObj);
 		}
 
 		// handle relative paths
@@ -74,18 +68,15 @@ public class AndroidFileSystemView implements FileSystemView {
 
 		logger.debug("getFile(), returning rel: '{}'", file);
 
-		return new AndroidFtpFile(new File(file), user);
+		return createFile(new File(file));
 	}
 
-	@Override
 	public boolean isRandomAccessible() throws FtpException {
 		logger.debug("isRandomAccessible()");
 		return true;
 	}
 
-	@Override
 	public void dispose() {
 		logger.debug("dispose()");
 	}
-
 }
---
+++ primitiveFTPd/src/org/primftpd/filesystem/FtpFileSystemView.java
@@ -0,0 +1,23 @@
+package org.primftpd.filesystem;
+
+import java.io.File;
+
+import org.apache.ftpserver.ftplet.FileSystemView;
+import org.apache.ftpserver.ftplet.User;
+
+public class FtpFileSystemView
+	extends AndroidFileSystemView<FtpFile, org.apache.ftpserver.ftplet.FtpFile>
+	implements FileSystemView
+{
+	private final User user;
+
+	public FtpFileSystemView(User user) {
+		this.user = user;
+	}
+
+	@Override
+	protected FtpFile createFile(File file)
+	{
+		return new FtpFile(file, user);
+	}
+}
