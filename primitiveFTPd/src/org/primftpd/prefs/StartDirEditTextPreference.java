1eb58bdfc20bd3ce223cb92e1c384a0389f8f258
wolpi <wolpi.dev@web.de>
2015-10-04 23:17:58 +0200

    Added validation and resetting option to server start dir preference. Validation still not very nice but OK. See issue#26.

--- primitiveFTPd/src/org/primftpd/prefs/StartDirEditTextPreference.java
+++ primitiveFTPd/src/org/primftpd/prefs/StartDirEditTextPreference.java
@@ -1,40 +1,64 @@
 package org.primftpd.prefs;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.TypedArray;
 import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Toast;
 
+import org.primftpd.R;
 import org.primftpd.util.Defaults;
 
-public class StartDirEditTextPreference extends EditTextPreference {
+import java.io.File;
 
-	public StartDirEditTextPreference(Context context) {
-		super(context);
-	}
-
+public class StartDirEditTextPreference extends EditTextPreference implements Preference.OnPreferenceChangeListener
+{
 	public StartDirEditTextPreference(
 			Context context,
 			AttributeSet attrs,
 			int defStyle) {
 		super(context, attrs, defStyle);
+        setOnPreferenceChangeListener(this);
 	}
 
 	public StartDirEditTextPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
-	}
-
-	@Override
-	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-		setText(Defaults.HOME_DIR.getAbsolutePath());
+        setOnPreferenceChangeListener(this);
 	}
 
 	@Override
 	protected Object onGetDefaultValue(TypedArray a, int index) {
 		return Defaults.HOME_DIR.getAbsolutePath();
+	}
+
+	@Override
+	protected void onPrepareDialogBuilder(AlertDialog.Builder builder) {
+		super.onPrepareDialogBuilder(builder);
+		builder.setNeutralButton(R.string.reset, this);
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		super.onClick(dialog, which);
+
+		if (which == DialogInterface.BUTTON_NEUTRAL) {
+			setText(Defaults.HOME_DIR.getAbsolutePath());
+		}
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		File file = new File(newValue.toString());
+		boolean valid = file.exists() && file.isDirectory();
+		if (!valid) {
+			Toast.makeText(
+				getContext(),
+				R.string.invalidDir,
+				Toast.LENGTH_LONG).show();
+		}
+		return valid;
 	}
 }
