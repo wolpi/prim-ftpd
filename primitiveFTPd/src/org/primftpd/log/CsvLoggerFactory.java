b264101a7b568d76e16b801a10a5a7a5a2977f08
wolpi <wolpi.dev@web.de>
2017-09-13 16:59:34 +0200

    added timestamp to logfile name and removing old logfiles, see GH issue #105

--- primitiveFTPd/src/org/primftpd/log/CsvLoggerFactory.java
+++ primitiveFTPd/src/org/primftpd/log/CsvLoggerFactory.java
@@ -1,11 +1,19 @@
 package org.primftpd.log;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
@@ -14,6 +22,10 @@ import android.os.Environment;
 
 public class CsvLoggerFactory implements ILoggerFactory
 {
+	public static final String LOGFILE_BASENAME = "prim-ftpd-log-";
+	private static final int NUM_LOGFILES_TO_KEEP = 3;
+	private static DateFormat FILENAME_DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+
 	private final Map<String, Logger> loggerMap = new HashMap<String, Logger>();
 
 	private final ILoggerFactory nopLoggerFactory;
@@ -36,7 +48,7 @@ public class CsvLoggerFactory implements ILoggerFactory
 				if (logger == null)
 				{
 					if (file == null) {
-						file = openFile();
+						file = findFile();
 					}
 
 					logger = new CsvLogger(name, file);
@@ -49,10 +61,46 @@ public class CsvLoggerFactory implements ILoggerFactory
 		return logger;
 	}
 
-	private PrintStream openFile() throws FileNotFoundException {
+	private PrintStream findFile() throws FileNotFoundException {
 		File dir = Environment.getExternalStorageDirectory();
-		File file = new File(dir, "prim-ftpd-log.csv");
+		cleanOldFiles(dir);
+
+		String filename = LOGFILE_BASENAME + FILENAME_DATEFORMAT.format(new Date()) + ".csv";
+		File file = new File(dir, filename);
 		// note: this is never closed
 		return new PrintStream(new FileOutputStream(file, true));
+	}
+
+	private void cleanOldFiles(File dir) {
+		File[] currentFiles = dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.startsWith(LOGFILE_BASENAME);
+			}
+		});
+		if (currentFiles.length >= NUM_LOGFILES_TO_KEEP) {
+			SortedSet<File> sorted = new TreeSet<File>(new Comparator<File>() {
+				@Override
+				public int compare(File file1, File file2) {
+					Long time1 = Long.valueOf(file1.lastModified());
+					Long time2 = Long.valueOf(file2.lastModified());
+					return time2.compareTo(time1);
+				}
+			});
+			for (File file : currentFiles) {
+				sorted.add(file);
+			}
+
+			int i=0;
+			Iterator<File> iterator = sorted.iterator();
+			while (iterator.hasNext()) {
+				if (i < NUM_LOGFILES_TO_KEEP) {
+					iterator.next().delete();
+				} else {
+					break;
+				}
+				i++;
+			}
+		}
 	}
 }
