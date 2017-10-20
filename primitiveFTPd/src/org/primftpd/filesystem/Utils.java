929f7ee63092ec5966dc9253223ac7310b9aaca3
wolpi <wolpi.dev@web.de>
2017-10-20 11:13:56 +0200

    refactored copied methods into static utils (added new file)

---
+++ primitiveFTPd/src/org/primftpd/filesystem/Utils.java
@@ -0,0 +1,39 @@
+package org.primftpd.filesystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Utils {
+
+    static List<String> normalizePath(String path) {
+        String[] parts = path.split("/");
+        List<String> result = new ArrayList<>();
+        for (String part : parts) {
+            if (".".equals(part) || "".equals(part)) {
+                continue;
+            } else if ("..".equals(part)) {
+                if (!result.isEmpty()) {
+                    result.remove(result.size() - 1);
+                }
+                continue;
+            } else {
+                result.add(part);
+            }
+        }
+        return result;
+    }
+
+    static String toPath(List<String> parts) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
+        int i = 0;
+        for (String part : parts) {
+            sb.append(part);
+            if (i < parts.size() - 1) {
+                sb.append("/");
+            }
+            i++;
+        }
+        return sb.toString();
+    }
+}
