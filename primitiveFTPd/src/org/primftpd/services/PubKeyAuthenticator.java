7e5bfd529d1f2dcd3e9652351a776c5d3a6e3dac
wolpi <wolpi.dev@web.de>
2019-09-22 00:58:46 +0200

    updated to api-level 28, integrated local copy of sshd into build, therefore adjusted PubKeyAuthenticator and commented in KeyParser, see GH issue #157

--- primitiveFTPd/src/org/primftpd/services/PubKeyAuthenticator.java
+++ primitiveFTPd/src/org/primftpd/services/PubKeyAuthenticator.java
@@ -5,10 +5,6 @@ import org.apache.sshd.server.session.ServerSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.math.BigInteger;
 import java.security.PublicKey;
 import java.util.List;
 
@@ -23,14 +19,14 @@ public class PubKeyAuthenticator implements PublickeyAuthenticator {
     }
 
     @Override
-    public boolean authenticate(String username, PublicKey key, ServerSession session) {
+    public boolean authenticate(String username, PublicKey clientKey, ServerSession session) {
         logger.debug("attempting pub key auth, user: {}, client key class: '{}'",
-                username, key.getClass().getName());
+                username, clientKey.getClass().getName());
         // never mind username
-        for (PublicKey configuredKey : pubKeys) {
-            boolean keyEquals = keysEqual(configuredKey, key);
+        for (PublicKey serverKey : pubKeys) {
+            boolean keyEquals = keysEqual(serverKey, clientKey);
             logger.debug("pub key auth, success: {}, server key class '{}'",
-                    keyEquals, configuredKey.getClass().getName());
+                    keyEquals, serverKey.getClass().getName());
             if (keyEquals) {
                 return true;
             }
@@ -38,75 +34,9 @@ public class PubKeyAuthenticator implements PublickeyAuthenticator {
         return false;
     }
     private boolean keysEqual(PublicKey serverKey, PublicKey clientKey) {
-        if (serverKey != null && clientKey != null) {
-            if (serverKey instanceof org.bouncycastle.jce.provider.JCEECPublicKey)
-            {
-                try {
-                    org.bouncycastle.jce.provider.JCEECPublicKey ecServerKey =
-                            (org.bouncycastle.jce.provider.JCEECPublicKey)serverKey;
-                    org.bouncycastle.math.ec.ECPoint serverKeyQ = ecServerKey.getQ();
-                    BigInteger serverKeyX = serverKeyQ.getAffineXCoord().toBigInteger();
-                    BigInteger serverKeyY = serverKeyQ.getAffineYCoord().toBigInteger();
-                    BigInteger[] clientKeyPoint = clientKeyPointQ(clientKey);
-                    return serverKeyX.equals(clientKeyPoint[0]) && serverKeyY.equals(clientKeyPoint[1]);
-                } catch (Exception e) {
-                    logger.error(
-                            "Could not get component of client key to compare with server key. Client key class: "
-                                    + clientKey.getClass().getName(),
-                            e);
-                }
-            } else {
-                return serverKey.equals(clientKey);
-            }
-        }
-        return false;
-    }
-    private BigInteger[] clientKeyPointQ(PublicKey clientKey)
-            throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException
-    {
-        Field field = clientKey.getClass().getDeclaredField("q");
-        field.setAccessible(true);
-        Object pointQ = field.get(clientKey);
-
-        BigInteger x = pointCoord("X", pointQ);
-        BigInteger y = pointCoord("Y", pointQ);
-        return new BigInteger[]{x, y};
-    }
-
-    private BigInteger pointCoord(String coord, Object point)
-            throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
-    {
-        Method getCoord = null;
-        String methodName = "get" + coord;
-        Method[] methods = point.getClass().getMethods();
-        for(Method method : methods) {
-//            Class[] paras = method.getParameterTypes();
-//            StringBuilder sb = new StringBuilder();
-//            sb.append("method: ");
-//            sb.append(method.getName());
-//            sb.append("(");
-//            String delimiter = "";
-//            for (Class para : paras) {
-//                sb.append(delimiter);
-//                sb.append(para.getName());
-//                delimiter = ", ";
-//            }
-//            sb.append(")");
-//            logger.info("{}", sb.toString());
-
-            if (methodName.equals(method.getName())) {
-                getCoord = method;
-                break;
-            }
-        }
-
-        if (getCoord == null) {
-            throw new NoSuchMethodException(methodName);
-        }
-
-        //Method getCoord = point.getClass().getDeclaredMethod("get" + coord);
-        Object fieldElementX = getCoord.invoke(point);
-        Method toBigInt = fieldElementX.getClass().getDeclaredMethod("toBigInteger");
-        return (BigInteger)toBigInt.invoke(fieldElementX);
+        // serverKey is an object from bouncy castle, see org.primftpd.pojo.KeyParser
+        // clientKey is an object from conscrypt (at least since api-level 28)
+        // fortunately conscrypt's equals() method supports comparision with bouncy castle objects
+        return clientKey.equals(serverKey);
     }
 }
