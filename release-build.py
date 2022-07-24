12a29bf4d2230502fe70e97841014e482926e334
wolpi <wolpi.dev@web.de>
2022-07-24 22:33:02 +0200

    extended release script to create special google play version without MANAGE_EXTERNAL_STORAGE

--- release-build.py
+++ release-build.py
@@ -7,155 +7,18 @@ import os
 import sys
 import json
 
-
-# check env vars
-keystorePath = ""
-releasesPath = ""
-try:
-    keystorePath = os.environ['PFTPD_KEYSTORE']
-    releasesPath = os.environ['PFTPD_RELEASES']
-except:
-    dummy=True
-
-if len(keystorePath) < 1:
-    print("env var PFTPD_KEYSTORE not set!")
-    sys.exit(-1)
-
-fullBuild = len(sys.argv) > 1 and sys.argv[1] == '--full'
-
-storePassword = getpass("store password: ")
-keyPassword = getpass("key password: ")
-
-# figure out version
-path = "primitiveFTPd/build.gradle"
-oldVersionCode = 0
-oldSnapshotVersion = ""
-content = ""
-with open(path, "r") as file:
-    content = file.read()
-
-    index = content.find("versionCode")
-    endIndex = content.find("\n", index)
-    line = content[index:endIndex]
-    oldVersionCode = line[line.find(" ")+1:len(line)]
-
-    index = content.find("versionName")
-    endIndex = content.find("\n", index)
-    line = content[index:endIndex]
-    oldSnapshotVersion = line[line.find("\"")+1:len(line)-1]
-
-newVersionCode = str(int(oldVersionCode)+1)
-releaseVersion = oldSnapshotVersion[0:oldSnapshotVersion.find("-")]
-major = oldSnapshotVersion[0:releaseVersion.find(".")]
-oldMinor = int(releaseVersion[releaseVersion.find(".")+1:len(releaseVersion)])
-newMinor = oldMinor + 1
-newVersion = major + "." + str(newMinor)
-newSnapshotVersion = newVersion + "-SNAPSHOT"
-
-
-if fullBuild:
-    print("oldVersionCode: " + oldVersionCode)
-    print("newVersionCode: " + newVersionCode)
-    print("oldSnapshotVersion: " + oldSnapshotVersion)
-    print("releaseVersion: " + releaseVersion)
-    print("major: " + major)
-    print("oldMinor: " + str(oldMinor))
-    print("newMinor: " + str(newMinor))
-    print("newVersion: " + newVersion)
-    print("newSnapshotVersion: " + newSnapshotVersion)
-    print()
-
-    # write release version in file
-    oldVersionCodeLine = "versionCode " + oldVersionCode
-    newVersionCodeLine = "versionCode " + newVersionCode
-    oldVersionLine = "versionName \"" + oldSnapshotVersion + "\""
-    releaseVersionLine = "versionName \"" + releaseVersion + "\""
-    newSnapshotVersionLine = "versionName \"" + newSnapshotVersion + "\""
-
-    content = content.replace(oldVersionLine, releaseVersionLine)
-    print("writing release version in file")
-    print()
-    file = open(path, "w")
-    file.write(content)
-    file.close()
-
-# run build
-print("running build")
-print()
-subprocess.run([
-    "./gradlew",
-    "clean",
-    "assembleRelease",
-    "-Pandroid.injected.signing.store.file=" + keystorePath,
-    "-Pandroid.injected.signing.key.alias=prim fptd sign key",
-    "-Pandroid.injected.signing.store.password=" + storePassword,
-    "-Pandroid.injected.signing.key.password=" + keyPassword
-], stdout=subprocess.PIPE, check=True)
-
-gitTag = "prim-ftpd-" + releaseVersion
-if fullBuild:
-    # commit version change
-    msg = "set version to " + releaseVersion
-    print("commiting, \"" + msg + "\"")
-    print()
+def doGithubUpload(githubToken, uploadUrl, apkPath, name):
     subprocess.run([
-        "git",
-        "add",
-        path
-    ], stdout=subprocess.PIPE, check=True)
-    subprocess.run([
-        "git",
-        "commit",
-        "-m",
-        msg
-    ], stdout=subprocess.PIPE, check=True)
+        "curl", "-v",
+        "-X", "POST",
+        "-H", "Authorization: token " + githubToken,
+        "-H", "Accept: application/vnd.github.v3+json",
+        "-H", "Content-Type: application/octet-stream",
+        "--data-binary", "@" + apkPath,
+              uploadUrl + "?name=" + "primitiveFTPd-" + name + ".apk",
+              ], stdout=subprocess.PIPE, check=True)
 
-    # tag
-    print("tagging")
-    print()
-    subprocess.run([
-        "git",
-        "tag",
-        "-a",
-        gitTag,
-        "-m",
-        "'" + gitTag + "'"
-    ], stdout=subprocess.PIPE, check=True)
-
-    # write new snapshot version in file
-    content = content.replace(oldVersionCodeLine, newVersionCodeLine)
-    content = content.replace(releaseVersionLine, newSnapshotVersionLine)
-    print("writing new snapshot version in file")
-    print()
-    file = open(path, "w")
-    file.write(content)
-    file.close()
-
-    # commit
-    msg = "set version to " + newSnapshotVersion + " and code to " + newVersionCode
-    print("commiting, \"" + msg + "\"")
-    print()
-    subprocess.run([
-        "git",
-        "add",
-        path
-    ], stdout=subprocess.PIPE, check=True)
-    subprocess.run([
-        "git",
-        "commit",
-        "-m",
-        msg
-    ], stdout=subprocess.PIPE, check=True)
-
-apkPath = "primitiveFTPd/build/outputs/apk/release/primitiveFTPd-release.apk"
-if len(releasesPath) > 0:
-    print("copy to releases dir")
-    targetPath = releasesPath + "/primitiveFTPd-" + (releaseVersion if fullBuild else oldSnapshotVersion) + ".apk"
-    copyfile(apkPath, targetPath)
-else:
-    print("releases dir not set, no copy")
-
-if fullBuild:
+def doRemoteGithubThings(apkPath, apkPathGoogleplay):
     # check if origin uses ssh url
     proc = subprocess.run([
         "git",
@@ -213,22 +76,6 @@ if fullBuild:
             gitTag
         ], stdout=subprocess.PIPE, check=True)
 
-        # create github release
-        print()
-        print("creating github release")
-        proc = subprocess.run([
-            "gh", "api",
-            "/repos/wolpi/prim-ftpd/releases",
-            "-X", "POST",
-            "-F", "tag_name=" + str(gitTag),
-            "-F", "name=" + str(gitTag),
-            "-F", "body=See [milestone](https://github.com/wolpi/prim-ftpd/milestone/" + str(milestoneNumber) + "?closed=1) for changes.\n",
-            "-F", "target_commitish=master",
-            "-F", "draft=false",
-            "-F", "prerelease=false",
-        ], stdout=subprocess.PIPE, check=True)
-        releaseObj = json.loads(proc.stdout)
-
         # close milestone
         print()
         print("closing github milestone")
@@ -237,7 +84,7 @@ if fullBuild:
             "-X", "PATCH",
             "-F", "state=closed",
             "/repos/wolpi/prim-ftpd/milestones/" + str(milestoneNumber),
-        ], stdout=subprocess.PIPE, check=True)
+            ], stdout=subprocess.PIPE, check=True)
 
         # create new milestone
         print()
@@ -247,7 +94,23 @@ if fullBuild:
             "-X", "POST",
             "-F", "title=" + str(newVersion),
             "/repos/wolpi/prim-ftpd/milestones",
-        ], stdout=subprocess.PIPE, check=True)
+                  ], stdout=subprocess.PIPE, check=True)
+
+        # create github release
+        print()
+        print("creating github release")
+        proc = subprocess.run([
+            "gh", "api",
+            "/repos/wolpi/prim-ftpd/releases",
+            "--method", "POST",
+            "-F", "tag_name=" + str(gitTag),
+            "-F", "name=" + str(gitTag),
+            "-F", "body=See [milestone](https://github.com/wolpi/prim-ftpd/milestone/" + str(milestoneNumber) + "?closed=1) for changes.\n",
+            "-F", "target_commitish=master",
+            "-F", "draft=false",
+            "-F", "prerelease=false",
+                  ], stdout=subprocess.PIPE, check=True)
+        releaseObj = json.loads(proc.stdout)
 
         # upload file
         print()
@@ -259,15 +122,8 @@ if fullBuild:
             uploadUrl = uploadUrl[0:index]
         print("using upload url: " + uploadUrl)
         print()
-        subprocess.run([
-            "curl", "-v",
-            "-X", "POST",
-            "-H", "Authorization: token " + githubToken,
-            "-H", "Accept: application/vnd.github.v3+json",
-            "-H", "Content-Type: application/octet-stream",
-            "--data-binary", "@" + apkPath,
-            uploadUrl + "?name=" + "primitiveFTPd-" + releaseVersion + ".apk",
-        ], stdout=subprocess.PIPE, check=True)
+        doGithubUpload(githubToken, uploadUrl, apkPath, releaseVersion)
+        doGithubUpload(githubToken, uploadUrl, apkPathGoogleplay, releaseVersion + "-googleplay")
         print()
         print("done")
         print()
@@ -283,3 +139,202 @@ if fullBuild:
         print("you should push and manage release as well as milestone!!!")
         print()
 
+
+def runBuild():
+    # run build
+    print("running build")
+    print()
+    subprocess.run([
+        "./gradlew",
+        "clean",
+        "assembleRelease",
+        "-Pandroid.injected.signing.store.file=" + keystorePath,
+        "-Pandroid.injected.signing.key.alias=prim fptd sign key",
+        "-Pandroid.injected.signing.store.password=" + storePassword,
+        "-Pandroid.injected.signing.key.password=" + keyPassword
+    ], stdout=subprocess.PIPE, check=True)
+
+
+def commit(msg, path):
+    print("commiting, \"" + msg + "\"")
+    print()
+    subprocess.run([
+        "git",
+        "add",
+        path
+    ], stdout=subprocess.PIPE, check=True)
+    subprocess.run([
+        "git",
+        "commit",
+        "-m",
+        msg
+    ], stdout=subprocess.PIPE, check=True)
+
+def gitTag(tag):
+    print("tagging")
+    print()
+    subprocess.run([
+        "git",
+        "tag",
+        "-a",
+        tag,
+        "-m",
+        "'" + tag + "'"
+    ], stdout=subprocess.PIPE, check=True)
+
+
+def copyToReleasesDir(isGoogleplayVersion):
+    apkPath = "primitiveFTPd/build/outputs/apk/release/primitiveFTPd-release.apk"
+    if len(releasesPath) > 0:
+        print("copy to releases dir")
+        targetPath = releasesPath + "/primitiveFTPd-" + \
+                     (releaseVersion if fullBuild else oldSnapshotVersion) + \
+                     ("-googleplay" if isGoogleplayVersion else "") + \
+                     ".apk"
+        copyfile(apkPath, targetPath)
+        return targetPath
+    else:
+        print("releases dir not set, no copy")
+
+
+
+def handleGooglePlayVersion(fullBuild, releaseVersion):
+    print("handling version for google play")
+    manifestPath = "primitiveFTPd/AndroidManifest.xml"
+    permissionExternalStorageEnabled = '<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />'
+    permissionExternalStorageOutcommented = '<!--<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />-->'
+
+    content = open(manifestPath, "r").read()
+    content = content.replace(permissionExternalStorageEnabled, permissionExternalStorageOutcommented)
+    file = open(manifestPath, "w")
+    file.write(content)
+    file.close()
+
+    runBuild()
+
+    if fullBuild:
+        # commit permission out commenting
+        msg = "removing permission to mange external storage for version " + releaseVersion
+        commit(msg, manifestPath)
+
+        # tag
+        gitTag("prim-ftpd-" + releaseVersion + "-google-play")
+
+    # enable permission again
+    content = content.replace(permissionExternalStorageOutcommented, permissionExternalStorageEnabled)
+    file = open(manifestPath, "w")
+    file.write(content)
+    file.close()
+
+    if fullBuild:
+        # commit
+        msg = "re-adding permission to mange external storage for next version after " + releaseVersion
+        commit(msg, manifestPath)
+
+
+# check env vars
+keystorePath = ""
+releasesPath = ""
+try:
+    keystorePath = os.environ['PFTPD_KEYSTORE']
+    releasesPath = os.environ['PFTPD_RELEASES']
+except:
+    dummy=True
+
+if len(keystorePath) < 1:
+    print("env var PFTPD_KEYSTORE not set!")
+    sys.exit(-1)
+
+fullBuild = len(sys.argv) > 1 and sys.argv[1] == '--full'
+
+storePassword = getpass("store password: ")
+keyPassword = getpass("key password: ")
+
+# figure out version
+pathBuildFile = "primitiveFTPd/build.gradle"
+oldVersionCode = 0
+oldSnapshotVersion = ""
+content = ""
+with open(pathBuildFile, "r") as file:
+    content = file.read()
+
+    index = content.find("versionCode")
+    endIndex = content.find("\n", index)
+    line = content[index:endIndex]
+    oldVersionCode = line[line.find(" ")+1:len(line)]
+
+    index = content.find("versionName")
+    endIndex = content.find("\n", index)
+    line = content[index:endIndex]
+    oldSnapshotVersion = line[line.find("\"")+1:len(line)-1]
+
+newVersionCode = str(int(oldVersionCode)+1)
+releaseVersion = oldSnapshotVersion[0:oldSnapshotVersion.find("-")]
+major = oldSnapshotVersion[0:releaseVersion.find(".")]
+oldMinor = int(releaseVersion[releaseVersion.find(".")+1:len(releaseVersion)])
+newMinor = oldMinor + 1
+newVersion = major + "." + str(newMinor)
+newSnapshotVersion = newVersion + "-SNAPSHOT"
+
+
+if fullBuild:
+    print("oldVersionCode: " + oldVersionCode)
+    print("newVersionCode: " + newVersionCode)
+    print("oldSnapshotVersion: " + oldSnapshotVersion)
+    print("releaseVersion: " + releaseVersion)
+    print("major: " + major)
+    print("oldMinor: " + str(oldMinor))
+    print("newMinor: " + str(newMinor))
+    print("newVersion: " + newVersion)
+    print("newSnapshotVersion: " + newSnapshotVersion)
+    print()
+
+    # write release version in file
+    oldVersionCodeLine = "versionCode " + oldVersionCode
+    newVersionCodeLine = "versionCode " + newVersionCode
+    oldVersionLine = "versionName \"" + oldSnapshotVersion + "\""
+    releaseVersionLine = "versionName \"" + releaseVersion + "\""
+    newSnapshotVersionLine = "versionName \"" + newSnapshotVersion + "\""
+
+    content = content.replace(oldVersionLine, releaseVersionLine)
+    print("writing release version in file")
+    print()
+    file = open(pathBuildFile, "w")
+    file.write(content)
+    file.close()
+
+runBuild()
+
+if fullBuild:
+    # commit version change
+    msg = "set version to " + releaseVersion
+    commit(msg, pathBuildFile)
+
+    # tag
+    gitTag("prim-ftpd-" + releaseVersion)
+
+    # google play version
+    apkPath = copyToReleasesDir(False)
+    handleGooglePlayVersion(fullBuild, releaseVersion)
+    apkPathGoogleplay = copyToReleasesDir(True)
+
+    # write new snapshot version in file
+    content = content.replace(oldVersionCodeLine, newVersionCodeLine)
+    content = content.replace(releaseVersionLine, newSnapshotVersionLine)
+    print("writing new snapshot version in file")
+    print()
+    file = open(pathBuildFile, "w")
+    file.write(content)
+    file.close()
+
+    # commit
+    msg = "set version to " + newSnapshotVersion + " and code to " + newVersionCode
+    commit(msg, pathBuildFile)
+else:
+    apkPath = copyToReleasesDir(False)
+    handleGooglePlayVersion(False, None)
+    apkPathGoogleplay= copyToReleasesDir(True)
+
+
+if fullBuild:
+    doRemoteGithubThings(apkPath, apkPathGoogleplay)
