fa14b8140487cac9b429d0f88838f3f279963161
wolpi <wolpi.dev@web.de>
2023-10-02 12:38:49 +0200

    wip: fixed warnings, part of #281

--- primitiveFTPd/src/org/primftpd/prefs/FtpPrefsFragment.java
+++ primitiveFTPd/src/org/primftpd/prefs/FtpPrefsFragment.java
@@ -2,6 +2,7 @@ package org.primftpd.prefs;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
-import androidx.annotation.NonNull;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
@@ -31,61 +31,82 @@ public class FtpPrefsFragment extends PreferenceFragmentCompat
 
 		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
 			logger.debug("disabling announce prefs, sdk: {}", Build.VERSION.SDK_INT);
-			PreferenceCategory prefCat = (PreferenceCategory) getPreferenceManager().findPreference("ftpPrefCatSystem");
+			PreferenceCategory prefCat = getPreferenceManager().findPreference("ftpPrefCatSystem");
 
-			Preference announcePref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_ANNOUNCE);
-			prefCat.removePreference(announcePref);
-
-			Preference announceNamePref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_ANNOUNCE_NAME);
-			prefCat.removePreference(announceNamePref);
-
-			prefCat = (PreferenceCategory) getPreferenceManager().findPreference("ftpPrefCatUi");
-			Preference showConnInfoPref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_SHOW_CONN_INFO);
-			prefCat.removePreference(showConnInfoPref);
-		}
-
-		// context
-		final Context context = getContext();
-
-		// text parameter for pub key auth pref
-		Resources res = getResources();
-		String text = String.format(res.getString(R.string.prefSummaryPubKeyAuth_v2), Defaults.pubKeyAuthKeyPath(context));
-		Preference pubKeyAuthPref = findPreference(LoadPrefsUtil.PREF_KEY_PUB_KEY_AUTH);
-		pubKeyAuthPref.setSummary(text);
-
-		// text parameter for logging pref
-		String textLogsPath = Defaults.homeDirScoped(context) + "/" + CsvLoggerFactory.LOGFILE_BASENAME + "*";
-		if (textLogsPath.contains("//")) {
-			textLogsPath = textLogsPath.replaceAll("//", "/");
-		}
-		String loggingText = String.format(res.getString(R.string.prefSummaryLoggingV2), textLogsPath);
-		Preference loggingPref = findPreference(LoadPrefsUtil.PREF_KEY_LOGGING);
-		loggingPref.setSummary(loggingText);
-
-		// create / remove notification when pref is toggled
-		Preference startStopNotificationPref = findPreference(LoadPrefsUtil.PREF_KEY_SHOW_START_STOP_NOTIFICATION);
-		startStopNotificationPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				if (Boolean.TRUE.equals(newValue)) {
-					NotificationUtil.createStartStopNotification(getActivity());
-				} else {
-					NotificationUtil.removeStartStopNotification(getActivity());
+			if (prefCat != null) {
+				Preference announcePref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_ANNOUNCE);
+				if (announcePref != null) {
+					prefCat.removePreference(announcePref);
 				}
-				return true;
+
+				Preference announceNamePref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_ANNOUNCE_NAME);
+				if (announceNamePref != null) {
+					prefCat.removePreference(announceNamePref);
+				}
+
+				prefCat = getPreferenceManager().findPreference("ftpPrefCatUi");
+				if (prefCat != null) {
+					Preference showConnInfoPref = getPreferenceManager().findPreference(LoadPrefsUtil.PREF_KEY_SHOW_CONN_INFO);
+					if (showConnInfoPref != null) {
+						prefCat.removePreference(showConnInfoPref);
+					}
+				}
 			}
-		});
+		}
+		final Context context = getContext();
+		if (context != null) {
+			// text parameter for pub key auth pref
+			Resources res = getResources();
+			String text = String.format(
+					res.getString(R.string.prefSummaryPubKeyAuth_v2),
+					Defaults.pubKeyAuthKeyPath(context));
+			Preference pubKeyAuthPref = findPreference(LoadPrefsUtil.PREF_KEY_PUB_KEY_AUTH);
+			if (pubKeyAuthPref != null) {
+				pubKeyAuthPref.setSummary(text);
+			}
+
+			// text parameter for logging pref
+			String textLogsPath = Defaults.homeDirScoped(context) + "/" + CsvLoggerFactory.LOGFILE_BASENAME + "*";
+			if (textLogsPath.contains("//")) {
+				textLogsPath = textLogsPath.replaceAll("//", "/");
+			}
+			String loggingText = String.format(
+					res.getString(R.string.prefSummaryLoggingV2),
+					textLogsPath);
+			Preference loggingPref = findPreference(LoadPrefsUtil.PREF_KEY_LOGGING);
+			if (loggingPref != null) {
+				loggingPref.setSummary(loggingText);
+			}
+
+			// create / remove notification when pref is toggled
+			Preference startStopNotificationPref = findPreference(LoadPrefsUtil.PREF_KEY_SHOW_START_STOP_NOTIFICATION);
+			if (startStopNotificationPref != null) {
+				startStopNotificationPref.setOnPreferenceChangeListener((preference, newValue) -> {
+					if (Boolean.TRUE.equals(newValue)) {
+						NotificationUtil.createStartStopNotification(context);
+					} else {
+						NotificationUtil.removeStartStopNotification(context);
+					}
+					return true;
+				});
+			}
+		}
 
 		// directory picker for choosing home dir
 		Preference startDirPref = findPreference(LoadPrefsUtil.PREF_KEY_START_DIR);
-		startDirPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(@NonNull Preference preference) {
-				File startDirVal = LoadPrefsUtil.startDir(startDirPref.getSharedPreferences());
-				Intent dirPickerIntent = Defaults.createPrefDirPicker(context, startDirVal, LoadPrefsUtil.PREF_KEY_START_DIR);
-				startDirPref.setIntent(dirPickerIntent);
+		if (startDirPref != null) {
+			startDirPref.setOnPreferenceClickListener(preference -> {
+				SharedPreferences sharedPrefs = startDirPref.getSharedPreferences();
+				if (sharedPrefs != null) {
+					File startDirVal = LoadPrefsUtil.startDir(sharedPrefs);
+					Intent dirPickerIntent = Defaults.createPrefDirPicker(
+							context,
+							startDirVal,
+							LoadPrefsUtil.PREF_KEY_START_DIR);
+					startDirPref.setIntent(dirPickerIntent);
+				}
 				return false;
-			}
-		});
+			});
+		}
 	}
 }
