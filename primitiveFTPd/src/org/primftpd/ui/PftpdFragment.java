cd6df64487b4cfd2f735d4264c42d7479f7f97b3
Hamid Safdari <hamidsafdari22@gmail.com>
2024-10-12 13:21:41 +0500

    ui(#387): refactor ui for better readability
    - left align items
    - add tools:text to layout files for better development
    - sort ips so IPv4 ips are on top

--- primitiveFTPd/src/org/primftpd/ui/PftpdFragment.java
+++ primitiveFTPd/src/org/primftpd/ui/PftpdFragment.java
@@ -23,7 +23,6 @@ import android.provider.DocumentsContract;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.UnderlineSpan;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -468,7 +467,6 @@ public class PftpdFragment extends Fragment implements RecreateLogger, RadioGrou
 			TextView textView = new TextView(container.getContext());
 			container.addView(textView);
 			textView.setText(displayText);
-			textView.setGravity(Gravity.CENTER_HORIZONTAL);
 			textView.setTextIsSelectable(true);
 		}
 
